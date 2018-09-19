e0a40e0c5fec9e126ac1ec25aacb2af76711800d
Sungmiii <christyksmile@gmail.com>
2018-09-19 19:53:06 +1200

    my pick game is working

--- project/devilFruit/main.js
+++ project/devilFruit/main.js
@@ -1,5 +1,6 @@
 let myImage = 0;
 let time;
+imageShow();
 
 /* function for imageslide  */
 function imageShow(){
@@ -15,49 +16,41 @@ function imageShow(){
 /* current image fruit info shows up */
 $("#randombtn").click(function(){
     clearTimeout(time);
-    
+ 
     function checkFruit(){
         let text;
-        let currentImage = $(".imgarea img");
-        //if(currentImage.style.display ="block"){}
-        switch(currentImage){
-            case currentImage.hasClass("gomu"):
-            text = "Gomu Gomu Fruit";
-            break;
-            case currentImage.hasClass("ito"):
-            text = "Ito Ito Fruit";
-            break;
-            case currentImage.hasClass("mera"):
-            text = "Mera Mera Fruit";
-            break;
-            case currentImage.hasClass("ope"):
-            text = "Ope Ope Fruit";
-            break;
-            case currentImage.hasClass("sara"):
-            text = "Sara Sara Fruit";
-            break;
-            case currentImage.hasClass("arttidevilfruti"):
-            text = "Artificial Devil Fruit";
-            break;
-            case currentImage.hasClass("awa"):
-            text = "Awa Awa Fruit";
-            break;
-            case currentImage.hasClass("bara"):
-            text = "Bara Bara Fruit";
-            break;
-            case currentImage.hasClass("batto"):
-            text = "Batto Batto Fruit";
-            break;
-            case currentImage.hasClass("gol"):
-            text = "Gol Gol Fruit";
-            break;
-            default:
-            text = "Nothing for you"
-        }
+        $('.devilfruit').each(function(){
+            if($(this).css('display')==='block' && $(this).hasClass("gol")){
+                text = "<h3>You pick the Gol Gol Fruit!</h3><p>You are able to telekinetically manipulate gold at will</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("batto")){
+                text = "<h3>You pick the Batto Batto Fruit!</h3><p>You are able to turn into a vampire, drink lots of blood!</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("bara")){
+                text = "<h3>You pick the Bara Bara Fruit!</h3><p>You are able to be immune to slashing attacks and to be able to split your own body apart into pieces and control the said pieces however they wish, mostly by levitating them away from your main body, your are a Splitting Human!!</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("awa")){
+                text = "<h3>You pick the Awa Awa Fruit!</h3><p>You are able to emit and control soap and bubbles that can not only clean off dirt but can also \"clean off\" power, you are a Soap Human!</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("arttidevilfruti")){
+                text = "<h3>You pick the Artificial Devil Fruit!</h3><p>This is man-made devil fruit and not sure the power, please let me know if you are aware of this fruit.</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("sara")){
+                text = "<h3>You pick the Sara Sara Fruit!</h3><p>You are able to turn into an axolotl hybrid and a full axolotl at will.</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("ope")){
+                text = "<h3>You pick the Ope Ope Fruit!</h3><p>You are able to create a spherical space or \"room\", in which you are able to complete control over the placement and orientation of the objects inside, you are a Free Modification Human!!</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("mera")){
+                text = "<h3>You pick the Mera Mera Fruit!</h3><p>You are able to create, control, and transform into fire!</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("ito")){
+                text = "<h3>You pick the Ito Ito Fruit!</h3><p>You are able to create and manipulate strings, you are a String Human!</p>";
+            }else if($(this).css('display')==='block' && $(this).hasClass("gomu")){
+                text = "<h3>You pick the Gomu Gomu Fruit!</h3><p>Your body has the properties of rubber, you are a Rubber Human!</p>";
+            }
+        })
         $("#fruitInfo").html(text)
     }
-    checkFruit();  
-})
+    checkFruit();
+});      
 
-imageShow();
+
+$("#replaybtn").click(function(){
+    imageShow();
+    $("#fruitInfo").html("")    
+});
+
 
